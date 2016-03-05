253e353ebd3bd7c2a58d8f0dc1ce927c18f7dbef
romanluk <rlukomskti@gmail.com>
2016-03-05 06:13:01 -0800

    Added new activity "Read"

--- app/src/main/java/com/lukomskyi/roman/waystospendyourtime/CallFriend.java
+++ app/src/main/java/com/lukomskyi/roman/waystospendyourtime/CallFriend.java
@@ -1,30 +1,44 @@
 package com.lukomskyi.roman.waystospendyourtime;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 public class CallFriend extends AppCompatActivity {
     ImageButton btnCall;
+    Button btnNext;
+    ImageView image;
+    Drawable drawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_friend);
-        btnCall = (ImageButton) findViewById(R.id.call_button);
+        btnCall = (ImageButton) findViewById(R.id.btn_call);
+        btnNext = (Button)findViewById(R.id.btn_call_next);
+        image = (ImageView)findViewById(R.id.call_boys);
+        drawable = image.getDrawable();
         btnCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_DIAL);
-            //    intent.setData(Uri.parse("tel:0123456789"));
                 startActivity(intent);
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CallFriend.this, ReadActivity.class);
+                CallFriend.this.startActivity(intent);
             }
         });
     }
